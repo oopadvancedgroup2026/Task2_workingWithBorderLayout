f2c6e78866401e5e0269c0c308a6b423de880002
Godfrey200 <godfreyjuma6969@gmail.com>
2026-03-09 01:49:33 +0300

    Commit-1 Created java Junit 5 Test class workingWithBorderLayoutTest
    In the new version on line 1, I have imported the org.junit.jupiter.api.Assertion package
    
    In the new version on line 6, I have generated a setup method with the @org.junit.jupiter.api.BeforeEach.
    
    In the new version on line 10, 14, 18, 22 and 26, I have generated void methods borderFrame, borderLabel, borderPanel, borderScroller and SubmitButton. All with @org.junit.jupiter.api.Test respectively

---
+++ src/workingWithBorderLayoutTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class workingWithBorderLayoutTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderFrame() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderLabel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderPanel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void borderScroller() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void SubmitButton() {
+    }
+}
