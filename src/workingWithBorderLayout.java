4b8846eab349ffed8cdcfb68ed92d82b55d3e318
Abdirahman070 <myare0706@gmail.com>
2026-03-08 13:51:03 +0300

    commit_2
    on OV of line number 27 i Fixed spelling in application title.

--- src/workingWithBorderLayout.java
+++ src/workingWithBorderLayout.java
@@ -24,7 +24,7 @@ public class workingWithBorderLayout {
 
 
     public  JLabel BorderLabel(){
-        JLabel BorderLabel = new JLabel("Application Tittle");
+        JLabel BorderLabel = new JLabel("Application Title");
         BorderLabel.setHorizontalAlignment(JLabel.CENTER);
 
         return BorderLabel;
