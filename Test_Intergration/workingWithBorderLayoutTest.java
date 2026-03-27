3a46cfa786d275983e2fcf71bbad4f9e7782f250
clementmungukende-Jakpanther <clementmungukende@gmail.com>
2026-03-27 21:38:37 +0300

    test2
    test with logic

--- Test_Intergration/workingWithBorderLayoutTest.java
+++ Test_Intergration/workingWithBorderLayoutTest.java
@@ -1,32 +1,53 @@
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class workingWithBorderLayoutTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
+    workingWithBorderLayout app = new workingWithBorderLayout();
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void borderFrame() {
+        JFrame BorderFrame = new JFrame("Borderlayout Demo");
+        BorderFrame.setSize(600, 400);
+        BorderFrame.setLayout(new BorderLayout());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void borderLabel() {
+        JLabel BorderLabel = new JLabel("Application Tittle");
+        BorderLabel.setHorizontalAlignment(JLabel.CENTER);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void borderPanel() {
+        JPanel Borderpanel = new JPanel();
+        Borderpanel.setLayout(new GridLayout(3,1));
+
+        JButton Button1 = new JButton("Option-1");
+        JButton Button2 = new JButton("Option-2");
+        JButton Button3 = new JButton("Option-3");
+
+        Borderpanel.add(Button1);
+        Borderpanel.add(Button2);
+        Borderpanel.add(Button3);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void borderScroller() {
+        JTextArea BorderTextArea = new JTextArea("Type here.....");
+        BorderTextArea.setLineWrap(true);
+        BorderTextArea.setWrapStyleWord(true);
+
+        JScrollPane BorderScroller = new JScrollPane(BorderTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void submitButton() {
+        JButton submitButton = new JButton("Submit");
     }
 }
