35f60c61b69837d9596bdedf721d9e96bef9f3ea
clementmungukende-Jakpanther <clementmungukende@gmail.com>
2026-04-03 13:26:34 +0300

    Update workingWithBorderLayoutTest.java

--- Test_Intergration/workingWithBorderLayoutTest.java
+++ Test_Intergration/workingWithBorderLayoutTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
-
 import javax.swing.*;
 import java.awt.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class workingWithBorderLayoutTest {
@@ -12,29 +10,45 @@ class workingWithBorderLayoutTest {
     @Test
     void borderFrame() {
         JFrame BorderFrame = new JFrame("Borderlayout Demo");
+        assertNotNull(BorderFrame);
+        assertTrue(BorderFrame.getLayout() instanceof BorderLayout);
         BorderFrame.setSize(600, 400);
-        BorderFrame.setLayout(new BorderLayout());
+        Dimension size = BorderFrame.getSize();
+        assertEquals(600, size.width);
+        assertEquals(400, size.height);
+
+
     }
 
     @Test
     void borderLabel() {
-        JLabel BorderLabel = new JLabel("Application Tittle");
-        BorderLabel.setHorizontalAlignment(JLabel.CENTER);
+        JLabel BorderLabel = new JLabel("Application Tittle", JLabel.CENTER);
+        assertNotNull(BorderLabel);
+        assertEquals("Application Tittle", BorderLabel.getText());
+        assertEquals(JLabel.CENTER, BorderLabel.getHorizontalAlignment());
     }
+
 
     @Test
     void borderPanel() {
-        JPanel Borderpanel = new JPanel();
-        Borderpanel.setLayout(new GridLayout(3,1));
+        JPanel Borderpanel = new JPanel(new GridLayout(1, 3)); // set GridLayout with 3 columns
+        Borderpanel.add(new JButton("Option-1"));
+        Borderpanel.add(new JButton("Option-2"));
+        Borderpanel.add(new JButton("Option-3"));
 
-        JButton Button1 = new JButton("Option-1");
-        JButton Button2 = new JButton("Option-2");
-        JButton Button3 = new JButton("Option-3");
+        assertNotNull(Borderpanel);
+        assertTrue(Borderpanel.getLayout() instanceof GridLayout);
+        assertEquals(3, Borderpanel.getComponentCount());
 
-        Borderpanel.add(Button1);
-        Borderpanel.add(Button2);
-        Borderpanel.add(Button3);
+        JButton b1 = (JButton) Borderpanel.getComponent(0);
+        JButton b2 = (JButton) Borderpanel.getComponent(1);
+        JButton b3 = (JButton) Borderpanel.getComponent(2);
+
+        assertEquals("Option-1", b1.getText());
+        assertEquals("Option-2", b2.getText());
+        assertEquals("Option-3", b3.getText());
     }
+
 
     @Test
     void borderScroller() {
@@ -42,12 +56,26 @@ class workingWithBorderLayoutTest {
         BorderTextArea.setLineWrap(true);
         BorderTextArea.setWrapStyleWord(true);
 
-        JScrollPane BorderScroller = new JScrollPane(BorderTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+        assertTrue(BorderTextArea.getLineWrap());
+        assertTrue(BorderTextArea.getWrapStyleWord());
+
+        JScrollPane scroller = new JScrollPane(BorderTextArea,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        assertNotNull(scroller);
+        assertEquals(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                scroller.getVerticalScrollBarPolicy());
+        assertEquals(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER,
+                scroller.getHorizontalScrollBarPolicy());
     }
+
 
     @Test
     void submitButton() {
         JButton submitButton = new JButton("Submit");
+        assertNotNull(submitButton);
+        assertEquals("Submit", submitButton.getText());
+
     }
 }
